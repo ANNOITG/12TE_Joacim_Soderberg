7fcffb6a1d08e77411273955fa3e79647272b8d0
Skyhighping <sky.high.ping@gmail.com>
2013-10-11 12:10:08 +0200

    Täcker uppgift 12.1 och 12.2 än så länge

---
+++ Programmering 1/Övning 12.1/Övning 12.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_12._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5]; //<-- Vektorn skapas
+
+            for (int i = 0; i < namn.Length; i++)
+            {
+                namn[i] = Console.ReadLine();
+            }//<-- slut på loopen
+
+            while(true)
+            {
+                Console.Clear();
+            for (int i = 0; i < namn.Length; i++) //<-- Loop för att skriva ut alla namnen
+            {
+                Console.WriteLine(namn[i]);//<-- Namnen skrivs ut 
+            }//<-- slut på loopen
+            
+            Console.WriteLine("Mata in önskad index som du vill ändra 0-4 eller stäng ner med q och sedan Enter");
+            string userInput = Console.ReadLine(); 
+            if ((userInput == "q") || (userInput == "Q" ))//<-- q och enter kommer stänga ner programmet
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                hint index = int.Parse(userInput);
+                namn[index] = Console.ReadLine();
+            }
+        }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
